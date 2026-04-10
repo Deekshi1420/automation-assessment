0b689ae24439b6671c38593c29e8f9ae3644518e
Deekshith <your-email@gmail.com>
2026-04-10 11:09:30 +0530

    final stable assertion fix

--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -7,8 +7,7 @@ test('Login test', async ({ page }) => {
   await loginPage.goto();
   await loginPage.login('standard_user', 'secret_sauce');
 
-  // 🔥 WAIT FOR PAGE LOAD (IMPORTANT)
   await page.waitForURL('**/inventory.html');
 
-  await expect(page).toHaveURL(/inventory/);
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });

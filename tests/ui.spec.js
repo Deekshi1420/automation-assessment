e4468ae7d425e384ad378344fdbec9aba2423495
Deekshith <your-email@gmail.com>
2026-04-10 11:04:18 +0530

    added login test case

--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -1,20 +1,11 @@
 const { test, expect } = require('@playwright/test');
 const LoginPage = require('../pages/loginPage');
 
-test('Login Test', async ({ page }) => {
-  const login = new LoginPage(page);
+test('Login test', async ({ page }) => {
+  const loginPage = new LoginPage(page);
 
-  await login.goto();
-  await login.login('standard_user', 'secret_sauce');
+  await loginPage.goto();
+  await loginPage.login('standard_user', 'secret_sauce');
 
   await expect(page).toHaveURL(/inventory/);
-});
-
-test('Invalid Login Test', async ({ page }) => {
-  const login = new LoginPage(page);
-
-  await login.goto();
-  await login.login('wrong_user', 'wrong_pass');
-
-  await expect(page).toHaveURL(/saucedemo/);
 });
